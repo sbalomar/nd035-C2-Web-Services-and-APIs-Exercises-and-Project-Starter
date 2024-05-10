6f7a0e90e8332a37a1211440c85bb9543e63cc61
sbalomar <sbalomar@tahakom.com>
2024-05-10 21:23:19 +0300

    pricing service

--- P02-VehiclesAPI/eureka/src/main/java/com/udacity/eureka/EurekaApplication.java
+++ P02-VehiclesAPI/eureka/src/main/java/com/udacity/eureka/EurekaApplication.java
@@ -1,19 +1,15 @@
-package com.udacity.pricing;
+package com.udacity.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * Creates a Spring Boot Application to run the Pricing Service.
- * TODO: Convert the application from a REST API to a microservice.
- */
 @SpringBootApplication
 @EnableEurekaServer
-public class PricingServiceApplication {
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PricingServiceApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 
 }
--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,39 @@
 package com.udacity.pricing;
 
 import org.junit.Test;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void findVehiclePrice() throws Exception {
+		mockMvc.perform(get("/services/price")
+				.param("vehicleId", "4"))
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.vehicleId").value(4))
+				.andDo(print());
+
+	}
 }
